76bc9f9b627c91499740193760d88b42227c03aa
Umugwaneza Cynthia <c.umugwanez@alustudent.com>
2024-10-17 15:42:27 +0200

    backend

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -13,14 +13,17 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 class Config:
     """Config class for your application, it deals with babel mostly"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
@@ -30,16 +33,34 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-@app.route('/', methods=['GET', 'POST'], strict_slashes=False)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     """Home page for your application"""
-    login_as = request.args.get('login_as')
-    if login_as:
-        g.user = users.get(int(login_as))
-    else:
-        g.user = None
-    return render_template('5-index.html')
+    login = False
+    if g.get('user'):
+        login = True
+    return render_template('5-index.html', login=login)
+
+
+def get_user():
+    """Get user information from users dict"""
+    try:
+        login_as = int(request.args.get('login_as'))
+        return users.get(int(login_as))
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """Before request"""
+    user = get_user()
+    print(user)
+    if user:
+        g.user = user
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
